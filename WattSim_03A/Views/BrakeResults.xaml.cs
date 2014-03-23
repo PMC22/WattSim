55968d0638ccbd874a3e4691d9e48448170f833c
PMC22 <phlpimcc@gmail.com>
2014-03-23 17:33:11 +0000

    Added brake results page

---
+++ WattSim_03A/Views/BrakeResults.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WattSim_03A.Views
+{
+    /// <summary>
+    /// Interaction logic for BrakeResults.xaml
+    /// </summary>
+    public partial class BrakeResults : Page
+    {
+        public BrakeResults()
+        {
+            InitializeComponent();
+        }
+    }
+}
